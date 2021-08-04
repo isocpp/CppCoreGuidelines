4a377581803172beea9d68248820f9febdb423b8
salmanahmad2 <hafizmaliksalman@gmail.com>
2021-08-04 15:35:57 +0500

    Add salman to list

--- dump.cpp
+++ dump.cpp
@@ -3,11 +3,9 @@
 //					- Andrew Sutton
 
 /*
-	Basic idea: provide concepts that define the interface(s) to different kinds of graphs so that you can do
-	basic graph operations and algoriths without knowing exactly which kind of graph it is and keep ignorant
-	about implementation details.
+
     
-    Basic design idea: do like the STL
+    
 */
 
 /*
